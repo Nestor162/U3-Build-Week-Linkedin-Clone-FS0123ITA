34f0ec2fa0d23c2e16617aa95f60561b7e852f23
Nestor162 <nestorcicardini@gmail.com>
2023-04-17 15:15:00 +0200

    Contenuto dinamico

--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -1,50 +1,21 @@
 import React from "react";
 import "./RightSidebar.css";
-import PeopleCard from "./PeopleCard";
 import ShowMore from "./ShowMore";
 import GroupCard from "./GroupCard";
+import FetchUsers from "./FetchUsers";
 
 function RightSidebar() {
   return (
     <aside className="r-sidebar d-flex flex-column mt-4">
       <section className="card mb-3 p-3">
         <h5 className="mt-3 fs-6 fw-semibold">People also viewed</h5>
-        <PeopleCard
-          name={"Margherita Dellucci"}
-          description={"Senior Web Developer"}
-          pic="https://unavatar.io/margherita"
-        />
-        <PeopleCard
-          name={"Ramiro Beneventi"}
-          description={"Co-Founder of Lorem Ipsum Inc"}
-          pic="https://unavatar.io/ramiro"
-        />
-        <PeopleCard
-          name={"Espedito Angelo"}
-          description={"Junior Full-Stack Developer"}
-          pic="https://unavatar.io/angelo"
-        />
-        <PeopleCard
-          name={"Daria Bucchos"}
-          description={"Recruiter at Example Company."}
-          pic="https://unavatar.io/daria"
-        />
+        <FetchUsers nResults={5} />
         <ShowMore />
       </section>
 
       <section className="card mb-3 p-3">
         <h5 className="mb-3 mt-3 fs-6 fw-semibold"> People you may know</h5>
-        <PeopleCard name={"Noemi Pisano"} description={"Junior Front-end developer"} pic="https://unavatar.io/noemi" />
-        <PeopleCard name={"Fosco Marcelo"} description={"Senior Back-end dev"} pic="https://unavatar.io/fosco" />
-        <PeopleCard name={"Ilario Capon"} description={"UI/UX Designer"} pic="https://unavatar.io/Ilario" />
-        <ShowMore />
-        <PeopleCard name={"Valerio Dellucci"} description={"Java developer"} pic="https://unavatar.io/Valerio" />
-        <ShowMore />
-        <PeopleCard
-          name={"Ermenegildo Palermo"}
-          description={"Senior mobile developer"}
-          pic="https://unavatar.io/Palermo"
-        />
+        <FetchUsers nResults={4} />
         <ShowMore />
       </section>
 

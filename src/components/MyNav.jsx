65ef203398505175107eee50ce65b5f855f99f32
Pierluigi Marzo <marzo.pierluigi991@gmail.com>
2023-04-17 15:19:45 +0200

    first

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 import {
-  Button,
   Col,
   Container,
   Form,
-  FormControl,
+  Dropdown,
   Nav,
   NavDropdown,
-  NavLink,
   Navbar,
-  Row,
 } from "react-bootstrap";
+import logo from "../assets/logo.svg";
+import home from "../assets/home.svg";
+import rete from "../assets/rete.svg";
+import work from "../assets/work.svg";
+import msg from "../assets/msg.svg";
+import aziende from "../assets/aziende.svg";
+import notifiche from "../assets/notifiche.svg";
+import avatar from "../assets/avatar.png";
 
 const MyNav = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container fluid className="px-5 ">
         <Container className="d-flex px-5">
-          <Navbar.Brand href="#">logo</Navbar.Brand>
+          <Navbar.Brand href="#home">
+            <img src={logo} alt="logo" width={50} height={50} />
+          </Navbar.Brand>
           <Navbar.Toggle className="ms-auto" aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Col xs={2}>
@@ -35,22 +42,82 @@ const MyNav = () => {
               style={{ maxHeight: "100px" }}
               navbarScroll
             >
-              <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Rete</Nav.Link>
-              <Nav.Link href="#action2">Lavoro</Nav.Link>
-              <Nav.Link href="#action2">Messaggistica</Nav.Link>
-              <Nav.Link href="#action2">Notifiche</Nav.Link>
+              <Nav.Link
+                className="nav-link d-flex flex-column justify-content-center align-items-center"
+                to="/"
+              >
+                <span>
+                  <img width={30} height={30} src={home} alt="home" />
+                </span>
+                <span>Home</span>
+              </Nav.Link>
+              <Nav.Link
+                className="nav-link d-flex flex-column justify-content-center align-items-center"
+                to="/"
+              >
+                <span>
+                  <img width={30} height={30} src={rete} alt="rete" />
+                </span>
+                <span>Rete</span>
+              </Nav.Link>
+              <Nav.Link
+                className="nav-link d-flex flex-column justify-content-center align-items-center"
+                to="/"
+              >
+                <span>
+                  <img width={30} height={30} src={work} alt="work" />
+                </span>
+                <span>Lavoro</span>
+              </Nav.Link>
+              <Nav.Link
+                className="nav-link d-flex flex-column justify-content-center align-items-center"
+                to="/"
+              >
+                <span>
+                  <img width={30} height={30} src={msg} alt="msg" />
+                </span>
+                <span>Messaggistica</span>
+              </Nav.Link>
+              <Nav.Link
+                className="nav-link d-flex flex-column justify-content-center align-items-center"
+                to="/"
+              >
+                <span>
+                  <img width={30} height={30} src={notifiche} alt="notifiche" />
+                </span>
+                <span>Notifiche</span>
+              </Nav.Link>
               <NavDropdown
                 className="border-end"
                 title="Tu"
                 id="navbarScrollingDropdown"
               >
-                <NavDropdown.Item href="#action3">Lavoro</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">
-                  Messaggistica
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action5">Notifiche</NavDropdown.Item>
+                <div className="d-flex flex-column justify-content-center align-items-center p-2">
+                  <img
+                    className="rounded-circle"
+                    src={avatar}
+                    alt="avatar"
+                    width={30}
+                    height={30}
+                  />
+                  <Dropdown.Toggle
+                    className="bg-transparent border-0 p-0"
+                    style={{ fontSize: "12px" }}
+                    id="dropdown-basic"
+                  >
+                    Tu
+                  </Dropdown.Toggle>
+                </div>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">
+                    Another action
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">
+                    Something else
+                  </Dropdown.Item>
+                </Dropdown.Menu>
               </NavDropdown>
 
               <NavDropdown title="Lavoro" id="navbarScrollingDropdown">

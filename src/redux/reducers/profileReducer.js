c0cbb1681a6db3da3e97a5154387c6098ae782c9
Marianna <marianna.albrici@gmail.com>
2023-04-18 16:46:33 +0200

    Fixed merged reducers

--- src/redux/reducers/profileReducer.js
+++ src/redux/reducers/profileReducer.js
@@ -1,92 +1,84 @@
 import {
-  SET_PROFILE_AREA,
-  SET_PROFILE_BIO,
-  SET_PROFILE_EMAIL,
-  SET_PROFILE_ID,
-  SET_PROFILE_IMG,
-  SET_PROFILE_LASTNAME,
-  SET_PROFILE_NAME,
-  SET_PROFILE_TITLE,
-  SET_PROFILE_USERNAME,
-  ADD_IMAGE,
-} from "../actions";
+	SET_PROFILE_AREA,
+	SET_PROFILE_BIO,
+	SET_PROFILE_EMAIL,
+	SET_PROFILE_ID,
+	SET_PROFILE_IMG,
+	SET_PROFILE_LASTNAME,
+	SET_PROFILE_NAME,
+	SET_PROFILE_TITLE,
+	SET_PROFILE_USERNAME,
+	ADD_IMAGE,
+} from '../actions';
 
 const initialState = {
-  name: "",
-  surname: "",
-  email: "",
-  bio: "",
-  title: "",
-  area: "",
-  username: "",
-  id: "",
-  img: "",
-  profileName: "",
-  profileLastname: "",
-  profileEmail: "",
-  profileBio: "",
-  profileTitle: "",
-  profileArea: "",
-  profileUsername: "",
-  profileId: "",
-  profileImg: "",
+	name: '',
+	surname: '',
+	email: '',
+	bio: '',
+	title: '',
+	area: '',
+	username: '',
+	id: '',
+	img: '',
+	data: '',
 };
 
 const profileReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case SET_PROFILE_NAME:
-      return {
-        ...state,
-        profileName: action.payload,
-      };
-    case SET_PROFILE_LASTNAME:
-      return {
-        ...state,
-        profileLastname: action.payload,
-      };
-    case SET_PROFILE_EMAIL:
-      return {
-        ...state,
-        profileEmail: action.payload,
-      };
-    case SET_PROFILE_BIO:
-      return {
-        ...state,
-        profileBio: action.payload,
-      };
-    case SET_PROFILE_TITLE:
-      return {
-        ...state,
-        profileTitle: action.payload,
-      };
-    case SET_PROFILE_AREA:
-      return {
-        ...state,
-        profileArea: action.payload,
-      };
-    case SET_PROFILE_USERNAME:
-      return {
-        ...state,
-        profileUsername: action.payload,
-      };
-    case SET_PROFILE_ID:
-      return {
-        ...state,
-        profileId: action.payload,
-      };
-    case SET_PROFILE_IMG:
-      return {
-        ...state,
-        profileImg: action.payload,
-      };
-    default:
-      return state;
-    case ADD_IMAGE:
-      return {
-        ...state,
-        data: action.payload,
-      };
-  }
+	switch (action.type) {
+		case SET_PROFILE_NAME:
+			return {
+				...state,
+				name: action.payload,
+			};
+		case SET_PROFILE_LASTNAME:
+			return {
+				...state,
+				surname: action.payload,
+			};
+		case SET_PROFILE_EMAIL:
+			return {
+				...state,
+				email: action.payload,
+			};
+		case SET_PROFILE_BIO:
+			return {
+				...state,
+				bio: action.payload,
+			};
+		case SET_PROFILE_TITLE:
+			return {
+				...state,
+				title: action.payload,
+			};
+		case SET_PROFILE_AREA:
+			return {
+				...state,
+				area: action.payload,
+			};
+		case SET_PROFILE_USERNAME:
+			return {
+				...state,
+				username: action.payload,
+			};
+		case SET_PROFILE_ID:
+			return {
+				...state,
+				id: action.payload,
+			};
+		case SET_PROFILE_IMG:
+			return {
+				...state,
+				img: action.payload,
+			};
+		default:
+			return state;
+		case ADD_IMAGE:
+			return {
+				...state,
+				data: action.payload,
+			};
+	}
 };
 
 export default profileReducer;

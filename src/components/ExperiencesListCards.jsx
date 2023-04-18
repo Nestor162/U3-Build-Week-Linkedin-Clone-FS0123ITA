89b984f9d30ab290dab9d9ca0fc0ea3e0eac84a4
Nestor162 <nestorcicardini@gmail.com>
2023-04-18 16:52:10 +0200

    Fetch delete experiences

--- src/components/ExperiencesListCards.jsx
+++ src/components/ExperiencesListCards.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import { Alert, Col, Row, Spinner } from "react-bootstrap";
 import { Trash } from "react-bootstrap-icons";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 function ExperiencesListCards() {
   const arrayExperiences = useSelector(state => state.experienceList.experiences);
-  console.log(arrayExperiences);
+  const userId = useSelector(state => state.personalProfile.profileId);
+
+  const deleteExperience = async id => {
+    try {
+      const response = await fetch(`https://striveschool-api.herokuapp.com/api/profile/${userId}/experiences/${id}`, {
+        method: "DELETE",
+        headers: {
+          authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+        }
+      });
+      if (response.ok) {
+        const data = await response.json();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDelete = id => {
+    deleteExperience(userId, id);
+  };
 
   return (
     <>
@@ -16,8 +36,14 @@ function ExperiencesListCards() {
               <Col xs={4} style={{ maxWidth: "fit-content" }} className="pt-3 ps-3">
                 {/* <img src={exp} className="img-fluid rounded-circle" alt={`img`} width="48" height="48" /> */}
               </Col>
-              <Col xs={11} className="position-relative">
-                <Trash className="position-absolute d-none" style={{ right: 0, top: "30%" }} />
+              <Col xs={11} className="position-relative exp-col">
+                <Trash
+                  className="position-absolute delete-button"
+                  size={20}
+                  onClick={() => {
+                    handleDelete(exp._id);
+                  }}
+                />
                 <div className="card-body">
                   <h5 className="card-title fw-semibold fs-6 mb-1">{exp.description}</h5>
                   <p className="card-text lh-0 mb-0">{exp.company}</p>
@@ -34,9 +60,8 @@ function ExperiencesListCards() {
         <div className="d-flex justify-content-center py-4">
           <Spinner />
         </div>
-        //
       )}
-      {arrayExperiences.length === 0 && <Alert>There aren't experiences yet</Alert>}
+      {/* {arrayExperiences.length === 0 && <Alert>There aren't experiences yet</Alert>} */}
     </>
   );
 }

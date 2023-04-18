fb4a05b69ffe8b1714d785b6ffdfe48229deb8da
Pierluigi Marzo <marzo.pierluigi991@gmail.com>
2023-04-18 17:05:45 +0200

    prima parte

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -7,16 +7,26 @@ export const SET_PROFILE_AREA = "SET_PROFILE_AREA";
 export const SET_PROFILE_USERNAME = "SET_PROFILE_USERNAME";
 export const SET_PROFILE_ID = "SET_PROFILE_ID";
 export const SET_PROFILE_IMG = "SET_PROFILE_IMG";
+export const ADD_IMAGE = "ADD_IMAGE";
+
+export const GET_POSTS = "GET_POSTS";
+
+export const ADD_POST = "ADD_POST";
+
+export const POST_IMAGE = "POST_IMAGE";
 
 export const GET_EXPERIENCES = "GET_EXPERIENCES";
 
-export const personalProfileFetch = async dispatch => {
+export const personalProfileFetch = async (dispatch) => {
   try {
-    const response = await fetch("https://striveschool-api.herokuapp.com/api/profile/me", {
-      headers: {
-        authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+    const response = await fetch(
+      "https://striveschool-api.herokuapp.com/api/profile/me",
+      {
+        headers: {
+          authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+        },
       }
-    });
+    );
     if (response.ok) {
       const data = await response.json();
       dispatch({ type: SET_PROFILE_NAME, payload: data.name });
@@ -36,11 +46,14 @@ export const personalProfileFetch = async dispatch => {
 
 export const experienceFetch = async (dispatch, userId) => {
   try {
-    const response = await fetch(`https://striveschool-api.herokuapp.com/api/profile/${userId}/experiences`, {
-      headers: {
-        authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+    const response = await fetch(
+      `https://striveschool-api.herokuapp.com/api/profile/${userId}/experiences`,
+      {
+        headers: {
+          authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+        },
       }
-    });
+    );
     if (response.ok) {
       const data = await response.json();
       dispatch({ type: GET_EXPERIENCES, payload: data });
@@ -48,4 +61,118 @@ export const experienceFetch = async (dispatch, userId) => {
   } catch (error) {
     console.log(error);
   }
+};
+
+export const addImageAsync = (data, userId) => {
+  let header = {
+    method: "POST",
+
+    headers: {
+      authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+    },
+  };
+
+  return async (dispatch, getState) => {
+    try {
+      let res = await fetch(
+        `https://striveschool-api.herokuapp.com/api/profile/${userId}/picture`,
+
+        { ...header, body: data }
+      );
+
+      if (res.ok) {
+        let addedImage = await res.json();
+
+        console.log(addedImage);
+
+        dispatch({
+          type: ADD_IMAGE,
+
+          payload: addedImage,
+        });
+      } else {
+        console.log("error");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+// export const getPostAsync = () => {
+
+// return async (dispatch, getState) => {
+
+// try {
+
+// let res = await fetch(
+
+// `https://striveschool-api.herokuapp.com/api/posts/ `,
+
+// getOptions("GET") //dichiaro getOptions per non doverlo ripetere per ogni metodo che vogliamo usare
+
+// );
+
+// if (res.ok) {
+
+// let fetchedPost = await res.json();
+
+// console.log(fetchedPost);
+
+// dispatch({
+
+// type: GET_POSTS,
+
+// payload: fetchedPost.sort(
+
+// (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+
+// ),
+
+// });
+
+// } else {
+
+// console.log("error");
+
+// }
+
+// } catch (error) {
+
+// console.log(error);
+
+// }
+
+// };
+
+// };
+
+export const addPostAsync = (handleClose, data) => {
+  return async (dispatch, getState) => {
+    try {
+      let res = await fetch(
+        `https://striveschool-api.herokuapp.com/api/posts/:postId`,
+
+        { ...getOptions("POST"), body: JSON.stringify(data) }
+      );
+
+      if (res.ok) {
+        let addedPost = await res.json();
+
+        console.log(addedPost);
+
+        handleClose();
+
+        dispatch({
+          type: ADD_POST,
+
+          payload: addedPost,
+        });
+      } else {
+        console.log("error");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 };

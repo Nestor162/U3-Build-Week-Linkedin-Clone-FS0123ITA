60b7eb7b9f2e9ab0edd271bc1d2946414be591eb
Pierluigi Marzo <marzo.pierluigi991@gmail.com>
2023-04-18 09:49:21 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import { Route, Routes } from 'react-router';
-import { BrowserRouter } from 'react-router-dom';
-import MainProfilePage from './components/MainProfilePage';
-import RightSidebar from './components/RightSidebar';
-import MyNav from './components/MyNav';
-import { Container } from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import MainProfilePage from "./components/MainProfilePage";
+import RightSidebar from "./components/RightSidebar";
+import MyNav from "./components/MyNav";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<MyNav />
-			<Container className="d-flex">
-				<Routes>
-					<Route path="/me" element={<MainProfilePage />} />
-				</Routes>
-				<RightSidebar />
-			</Container>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <MyNav />
+      <Container className="d-flex">
+        <Routes>
+          <Route path="/me" element={<MainProfilePage />} />
+        </Routes>
+        <RightSidebar />
+      </Container>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;

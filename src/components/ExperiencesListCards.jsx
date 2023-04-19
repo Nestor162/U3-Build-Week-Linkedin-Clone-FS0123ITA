6a64fe7244a9a7dc756258bd8e7a38ad11321eaf
Nestor162 <nestorcicardini@gmail.com>
2023-04-19 09:37:29 +0200

    Fix fetch delete

--- src/components/ExperiencesListCards.jsx
+++ src/components/ExperiencesListCards.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Alert, Col, Row, Spinner } from "react-bootstrap";
+import React, { useState } from "react";
+import { Alert, Button, Col, Modal, Row, Spinner } from "react-bootstrap";
 import { Trash } from "react-bootstrap-icons";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -17,14 +17,21 @@ function ExperiencesListCards() {
       });
       if (response.ok) {
         const data = await response.json();
+        console.log(data);
       }
     } catch (error) {
       console.log(error);
     }
   };
 
-  const handleDelete = id => {
-    deleteExperience(userId, id);
+  /* Modale conferma eliminazione */
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleAccept = id => {
+    deleteExperience(id);
+    setShow(false);
   };
 
   return (
@@ -37,13 +44,32 @@ function ExperiencesListCards() {
                 {/* <img src={exp} className="img-fluid rounded-circle" alt={`img`} width="48" height="48" /> */}
               </Col>
               <Col xs={11} className="position-relative exp-col">
-                <Trash
-                  className="position-absolute delete-button"
-                  size={20}
-                  onClick={() => {
-                    handleDelete(exp._id);
-                  }}
-                />
+                <Trash className="position-absolute delete-button" size={20} onClick={handleShow} />
+                <Modal show={show} onHide={handleClose}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                    Are you sure that you want to delete:{" "}
+                    <span>
+                      <strong className="d-block">{exp.company}</strong>
+                      <em className="d-block">{exp.description}</em>
+                    </span>
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                      Cancel
+                    </Button>
+                    <Button
+                      variant="danger"
+                      onClick={e => {
+                        handleAccept(exp._id);
+                      }}
+                    >
+                      Delete
+                    </Button>
+                  </Modal.Footer>
+                </Modal>
                 <div className="card-body">
                   <h5 className="card-title fw-semibold fs-6 mb-1">{exp.description}</h5>
                   <p className="card-text lh-0 mb-0">{exp.company}</p>

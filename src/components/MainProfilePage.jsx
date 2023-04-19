86e33710f8aa9575377af1c74604230b128656dd
Nestor162 <nestorcicardini@gmail.com>
2023-04-19 17:49:59 +0200

    Fix sidebar

--- src/components/MainProfilePage.jsx
+++ src/components/MainProfilePage.jsx
@@ -1,22 +1,24 @@
-import { Container } from 'react-bootstrap';
-import ImageProfile from './ImageProfile';
-import MainProfileActivity from './MainProfileActivity';
-import MainProfileAnalytics from './MainProfileAnalytics';
-import MainProfileSection from './MainProfileSection';
-import MainProfileExperiences from './MainProfileExperiences';
+import { Container } from "react-bootstrap";
+import ImageProfile from "./ImageProfile";
+import MainProfileActivity from "./MainProfileActivity";
+import MainProfileAnalytics from "./MainProfileAnalytics";
+import MainProfileSection from "./MainProfileSection";
+import MainProfileExperiences from "./MainProfileExperiences";
+import RightSidebar from "./RightSidebar";
 
 const MainProfilePage = () => {
-	return (
-		<>
-			<Container>
-				<MainProfileSection />
-				<MainProfileAnalytics />
-				<MainProfileActivity />
-				<MainProfileExperiences />
-				<ImageProfile />
-			</Container>
-		</>
-	);
+  return (
+    <>
+      <Container>
+        <MainProfileSection />
+        <MainProfileAnalytics />
+        <MainProfileActivity />
+        <MainProfileExperiences />
+        <ImageProfile />
+      </Container>
+      <RightSidebar />
+    </>
+  );
 };
 
 export default MainProfilePage;

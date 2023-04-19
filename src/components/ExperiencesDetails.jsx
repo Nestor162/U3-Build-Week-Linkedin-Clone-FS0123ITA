64a379cb159401b65aba3344738a6c8fc76daccc
Nestor162 <nestorcicardini@gmail.com>
2023-04-19 13:06:02 +0200

    Aggiornata pagina experience

--- src/components/ExperiencesDetails.jsx
+++ src/components/ExperiencesDetails.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import { Alert, Button, Card, Col, Container, Modal, Row } from "react-bootstrap";
-import { ArrowLeft, Trash } from "react-bootstrap-icons";
+import { Alert, Button, Card, Col, Container, Form, Modal, Row } from "react-bootstrap";
+import { ArrowLeft, Pencil, Trash } from "react-bootstrap-icons";
 import { useDispatch, useSelector } from "react-redux";
 import { experienceFetch } from "../redux/actions";
 import { Link } from "react-router-dom";
 
-function ExperiencesDetails() {
+function ExperiencesListCards() {
   const arrayExperiences = useSelector(state => state.experienceList.experiences);
   const userId = useSelector(state => state.personalProfile.id);
 
@@ -28,10 +28,45 @@ function ExperiencesDetails() {
     }
   };
 
+  const editExperience = async idExp => {
+    try {
+      const response = await fetch(
+        `https://striveschool-api.herokuapp.com/api/profile/${userId}/experiences/${idExp}`,
+        {
+          method: "PUT",
+          headers: {
+            authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(experienceInfo)
+        }
+      );
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   /* Modale conferma eliminazione */
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const [show2, setShow2] = useState(false);
+  const handleClose2 = () => setShow2(false);
+  const [idExp, setIdExp] = useState("");
+
+  const [experienceInfo, setExperienceInfo] = useState({
+    role: "",
+    company: "",
+    startDate: "",
+    endDate: "",
+    description: "",
+    area: ""
+  });
 
   const handleAccept = async id => {
     await deleteExperience(id);
@@ -39,10 +74,16 @@ function ExperiencesDetails() {
     await experienceFetch(dispatch, userId);
   };
 
+  const handleAcceptEdit = async () => {
+    await editExperience(idExp);
+    handleClose2();
+    await experienceFetch(dispatch, userId);
+  };
+
   return (
     <Container className="mainProfileContainer">
-      <Card className="mainProfileCard ">
-        <h4 className="p-4">Experiences</h4>
+      <Card className="mainProfileCard">
+        <h5 className="p-4">Experiences</h5>
         {arrayExperiences && arrayExperiences.length > 0 ? (
           arrayExperiences.map(exp => {
             return (
@@ -51,10 +92,26 @@ function ExperiencesDetails() {
                   {/* <img src={exp} className="img-fluid rounded-circle" alt={`img`} width="48" height="48" /> */}
                 </Col>
                 <Col xs={11} className="position-relative exp-col">
-                  <Trash className="position-absolute delete-button" size={20} onClick={handleShow} />
+                  <Trash className="position-absolute delete-button" size={20} onClick={handleShow} />{" "}
+                  <Pencil
+                    size={20}
+                    className="me-4 position-absolute delete-button edit-button"
+                    onClick={() => {
+                      setExperienceInfo({
+                        role: exp.role,
+                        company: exp.company,
+                        startDate: exp.startDate,
+                        endDate: exp.endDate,
+                        description: exp.description,
+                        area: exp.area
+                      });
+                      setShow2(true);
+                      setIdExp(exp._id);
+                    }}
+                  />
                   <Modal show={show} onHide={handleClose}>
                     <Modal.Header closeButton>
-                      <Modal.Title>Modal heading</Modal.Title>
+                      <Modal.Title>DELETE EXPERIENCE</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
                       Are you sure that you want to delete:{" "}
@@ -78,12 +135,17 @@ function ExperiencesDetails() {
                     </Modal.Footer>
                   </Modal>
                   <div className="card-body">
-                    <h5 className="card-title fw-semibold fs-6 mb-1">{exp.description}</h5>
-                    <p className="card-text lh-0 mb-0">{exp.company}</p>
+                    <h5 className="card-title fw-semibold fs-6 mb-1">{exp.role}</h5>
+                    <p className="mb-0">
+                      <small className="card-text lh-0 mb-0">{exp.company}</small>
+                    </p>
+
                     <small className="text-secondary lh-1">
                       {new Date(exp.startDate).toLocaleDateString()} -{" "}
                       {exp.endDate ? new Date(exp.endDate).toLocaleDateString() : "Present"}
                     </small>
+
+                    <p className="card-title mt-2">{exp.description}</p>
 
                     <p>{arrayExperiences.description}</p>
                   </div>
@@ -98,9 +160,97 @@ function ExperiencesDetails() {
         <p className="toActivityPage my-2">
           <Link to={"/me"}> Go back to profile page</Link> <ArrowLeft />
         </p>
+        <Modal show={show2} onHide={handleClose2}>
+          <Modal.Header closeButton>
+            <Modal.Title>EDIT EXPERIENCE</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Edit your experience and click on "save changes"
+            <Form>
+              <Form.Group className="mb-3" controlId="role">
+                <Form.Label>Role</Form.Label>
+                <Form.Control
+                  onChange={e => {
+                    setExperienceInfo({ ...experienceInfo, role: e.target.value });
+                  }}
+                  value={experienceInfo.role}
+                  type="text"
+                  placeholder="Enter your role"
+                  autoFocus
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="company">
+                <Form.Label>Company</Form.Label>
+                <Form.Control
+                  onChange={e => {
+                    setExperienceInfo({ ...experienceInfo, company: e.target.value });
+                  }}
+                  value={experienceInfo.company}
+                  type="text"
+                  placeholder="Enter the Company's name"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="Sdate">
+                <Form.Label>Start Date</Form.Label>
+                <Form.Control
+                  onChange={e => {
+                    setExperienceInfo({ ...experienceInfo, startDate: e.target.value });
+                  }}
+                  value={experienceInfo.startDate}
+                  type="date"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="Edate">
+                <Form.Label>End Date (can be empty)</Form.Label>
+                <Form.Control
+                  onChange={e => {
+                    setExperienceInfo({ ...experienceInfo, endDate: e.target.value });
+                  }}
+                  value={experienceInfo.endDate}
+                  type="date"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="description">
+                <Form.Label>Description</Form.Label>
+                <Form.Control
+                  onChange={e => {
+                    setExperienceInfo({ ...experienceInfo, description: e.target.value });
+                  }}
+                  value={experienceInfo.description}
+                  type="text"
+                  placeholder="Describe your experience"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="Area">
+                <Form.Label>Area</Form.Label>
+                <Form.Control
+                  onChange={e => {
+                    setExperienceInfo({ ...experienceInfo, area: e.target.value });
+                  }}
+                  value={experienceInfo.area}
+                  type="text"
+                  placeholder="Enter your country name"
+                />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose2}>
+              Cancel
+            </Button>
+            <Button
+              variant="success"
+              onClick={() => {
+                handleAcceptEdit();
+              }}
+            >
+              Save changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </Card>
     </Container>
   );
 }
 
-export default ExperiencesDetails;
+export default ExperiencesListCards;
